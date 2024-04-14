0003e01d5834a83f951752cf4b3e92e7fc85ed54
Toavina Mathieu Randrianjafy <mathieurandrianjafy@gmail.com>
2024-04-14 23:36:10 +0300

    Page qui liste tous les comptes

---
+++ src/main/java/mg/itu/toavina/tpbanque/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.toavina.tpbanque.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author toavi
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
